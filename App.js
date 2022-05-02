70c28c5c711dc28a170fe20a5401508086447bd6
prabhjotsumman <prabhjot.nith@gmail.com>
2022-05-02 14:52:03 -0400

    Tabs added in App

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
-import { Colors } from 'react-native-ui-lib';
+import { Colors, TabController } from 'react-native-ui-lib';
 
 import Login from './src/Password';
 import PostNews from './src/PostNews';
@@ -14,12 +14,39 @@ const App = () => {
   };
 
   return (
-    <SafeAreaView>
+    <View>
       <View>
         <Text style={[styles.bannerText]}>Short News Provider</Text>
       </View>
-      {!auth ? <Login onChange={handleLogin} /> : <PostNews />}
-    </SafeAreaView>
+      {/* {!auth ? <Login onChange={handleLogin} /> : <PostNews />} */}
+      <TabController
+        asCarousel
+        items={[
+          { label: 'Publish News' },
+          { label: 'Update News' },
+          { label: 'Third' },
+        ]}>
+        <TabController.TabBar />
+        <TabController.PageCarousel
+          style={{
+            // backgroundColor: 'cyan',
+            borderWidth: 2,
+            height: '86%',
+            // paddingBottom: '10%',
+          }}>
+          <TabController.TabPage index={0}>
+            {/* {!auth ? <Login onChange={handleLogin} /> : <PostNews />} */}
+            <PostNews />
+          </TabController.TabPage>
+          <TabController.TabPage index={1}>
+            <Text>This is the page.. 2</Text>
+          </TabController.TabPage>
+          <TabController.TabPage index={2}>
+            <Text>This is the page... 3</Text>
+          </TabController.TabPage>
+        </TabController.PageCarousel>
+      </TabController>
+    </View>
   );
 };
 
