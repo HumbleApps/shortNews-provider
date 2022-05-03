0b84c89e5ff3bbfeb5760f7199f1b9c894f8112a
prabhjotsumman <prabhjot.nith@gmail.com>
2022-05-03 15:39:08 -0400

    NewsProvider updated

--- src/NewsProvider/Provider.js
+++ src/NewsProvider/Provider.js
@@ -9,26 +9,55 @@ const Provider = ({ children }) => {
 
   //   getDataFromStore
   const getDataFromStore = async () => {
-    const allNews = await ref.get();
+    try {
+      const allNews = await ref.get();
 
-    const tmpData = [];
-    allNews.forEach(doc => {
-      tmpData.push({ id: doc.id, ...doc.data() });
-    });
-    setNewsData(tmpData);
+      const tmpData = [];
+      allNews.forEach(doc => {
+        tmpData.push({ id: doc.id, ...doc.data() });
+      });
+      tmpData.sort((a, b) => b.timestamp - a.timestamp);
+      setNewsData(tmpData);
+    } catch (error) {
+      return false;
+    }
   };
 
   //  addDataToStore
   const addDataToStore = async payload => {
-    const res = await ref.add(payload);
-    console.log(res.id);
-    return res.id;
+    try {
+      const res = await ref.add(payload);
+      console.log(res.id);
+      const updatedData = [...newsData, { id: res.id, ...payload }];
+      updatedData.sort((a, b) => b.timestamp - a.timestamp);
+      setNewsData(updatedData);
+      return res.id;
+    } catch (error) {
+      return false;
+    }
+  };
+
+  const updateDataToStore = async (docID, payload) => {
+    try {
+      await ref.doc(docID).update(payload);
+      const remainingItems = newsData.filter(item => item?.id !== docID);
+      const updatedData = [...remainingItems, { ...payload, id: docID }];
+      updatedData.sort((a, b) => b.timestamp - a.timestamp);
+      setNewsData(updatedData);
+      return true;
+    } catch (error) {
+      return false;
+    }
   };
 
   const deleteDataFromStore = id => {
-    ref.doc(id).delete();
-    const remainingData = newsData.filter(doc => doc.id !== id);
-    setNewsData(remainingData);
+    try {
+      ref.doc(id).delete();
+      const remainingData = newsData.filter(doc => doc.id !== id);
+      setNewsData(remainingData);
+    } catch (error) {
+      return false;
+    }
   };
 
   useEffect(() => {
@@ -42,6 +71,7 @@ const Provider = ({ children }) => {
         addDataToStore,
         getDataFromStore,
         deleteDataFromStore,
+        updateDataToStore,
       }}>
       {children}
     </Context.Provider>
